09ac6af4814fbe5524d5821ff16c904bca986a97
Cheri <66389113+cheri@users.noreply.github.com>
2020-06-12 16:36:36 -0500

    created new project

---
+++ myFirstCSharpProgram/myFirstCSharpProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myFirstCSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who is Dr Who");
+            string answer = Console.ReadLine(); 
+
+            Console.WriteLine("your Answer Is:"+" " + answer);
+        }
+    }
+}
